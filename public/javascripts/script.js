05d38bf1e2d32f71ffe8e446090f60af6f09541b
Satoshi Ebisawa <e.satoshi@gmail.com>
2011-12-04 01:11:26 +0900

    add debug messaging test

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,11 @@
 $(function() {
-  console.log('loaded!');
+  Pusher.log = function(message) {
+    if (window.console && window.console.log) window.console.log(message);
+  };
+  
+  var pusher = new Pusher(window.pusher_key);
+  var channel = pusher.subscribe('test_channel');
+  channel.bind('my_event', function(data) {
+    alert(data);
+  });
 });
