a3089845faa211eb8074020a2234e949954bed80
Satoshi Ebisawa <e.satoshi@gmail.com>
2011-12-04 01:06:23 +0900

    add pusher keys

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 # coding: utf-8
+require 'yaml'
+require 'sinatra/reloader' if development?
 
 configure do
   enable :session
+  pusher_conf = YAML::load_file('./config/pusher.yml')[ENV['RACK_ENV']]
+  %w(app_id key secret).each do |k|
+    Pusher.send("#{k}=", pusher_conf[k])
+  end
 end
 
 helpers do
