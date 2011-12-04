0431054c5c6a9db8e94b14849beeeff2dcd3c3d6
Satoshi Ebisawa <e.satoshi@gmail.com>
2011-12-04 11:54:58 +0900

    add twitter auth

--- app.rb
+++ app.rb
@@ -1,12 +1,18 @@
 # coding: utf-8
 require 'yaml'
+require 'pp'
 require 'sinatra/reloader' if development?
 
 configure do
-  enable :session
+  enable :sessions
   pusher_conf = YAML::load_file('./config/pusher.yml')[ENV['RACK_ENV']]
   %w(app_id key secret).each do |k|
     Pusher.send("#{k}=", pusher_conf[k])
+  end
+
+  oauth_conf = YAML::load_file('./config/oauth.yml')[ENV['RACK_ENV']]['twitter']
+  use OmniAuth::Builder do
+    provider :twitter, oauth_conf['key'], oauth_conf['secret']
   end
 end
 
@@ -21,6 +27,10 @@ end
 
 get '/' do
   erb :index
+end
+
+get '/auth/twitter/callback' do
+  pp env['omniauth.auth']
 end
 
 # GET /test?message=helloworld
