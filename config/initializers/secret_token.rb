f3c242554490d1fdd84caf61fdedf02f964456bc
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-01-22 19:34:39 +0900

    rails new slidesync -T --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slidesync::Application.config.secret_token = 'b68184f12610f48b3620ad34180674350e31e45d6b7a5d6d2a149de7317061d625da62b6872374ebb33db58981907f184d3f295c042095afef1fd245519e66c1'
