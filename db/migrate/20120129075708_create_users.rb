58473ca9117acebbb0be493ba0fdf1cec9f56c46
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-01-29 16:57:49 +0900

    r g model User provider uid name nickname icon_url

---
+++ db/migrate/20120129075708_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :name
+      t.string :nickname
+      t.string :icon_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/fabricators/user_fabricator.rb
@@ -0,0 +1,7 @@
+Fabricator(:user) do
+  provider "MyString"
+  uid "MyString"
+  name "MyString"
+  nickname "MyString"
+  icon_url "MyString"
+end
