59585ce312f3e712fe04ebcb401766d4104e4494
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-03-04 11:32:16 +0900

    User has_many :rooms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :rooms
+  
   def self.find_or_create_by_auth_hash(auth)
     user = User.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
     user.update_attributes(
