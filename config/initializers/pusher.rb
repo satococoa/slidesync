343009147048ffb18d5c427d846c5fe4b50185a4
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-04-18 21:53:02 +0900

    delete pusher settings for production

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,3 +1,5 @@
-Pusher.app_id = Settings.pusher.app_id
-Pusher.key = Settings.pusher.key
-Pusher.secret = Settings.pusher.secret
+if Rails.env.production?
+  Pusher.app_id = Settings.pusher.app_id
+  Pusher.key = Settings.pusher.key
+  Pusher.secret = Settings.pusher.secret
+end
